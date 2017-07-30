3f3ee93fb7b65ded500d33e9a6596a243224c72a
Ivyn San Pedro <isanpedro@gmail.com>
2017-07-30 15:41:21 -0400

    Add the TimeDistributed wrapper and the BatchNormalization layer

--- sample_models.py
+++ sample_models.py
@@ -25,12 +25,12 @@ def rnn_model(input_dim, units, activation, output_dim=29):
     # Main acoustic input
     input_data = Input(name='the_input', shape=(None, input_dim))
     # Add recurrent layer
-    simp_rnn = GRU(units, activation=activation,
+    simp_rnn = LSTM(units, activation=activation,
         return_sequences=True, implementation=2, name='rnn')(input_data)
     # TODO: Add batch normalization 
-    bn_rnn = ...
+    bn_rnn = BatchNormalization()(simp_rnn)
     # TODO: Add a TimeDistributed(Dense(output_dim)) layer
-    time_dense = ...
+    time_dense = TimeDistributed(Dense(output_dim))(bn_rnn)
     # Add softmax activation layer
     y_pred = Activation('softmax', name='softmax')(time_dense)
     # Specify the model
